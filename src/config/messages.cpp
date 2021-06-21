8cdbe54301c4eb3abcc80b295af6be3d8065deda
egorsobolev2000 <web-disain@mail.ua>
2021-06-22 00:51:49 +0300

    Обработка флагов + инициализация бота

---
+++ src/config/messages.cpp
@@ -0,0 +1,2 @@
+char ERROR__TOKEN_UNDEFINED[] = "Токен не был указан в качестве параметра";
+char ERROR__TOKEN_INCORRECTLY[] = "Токен указан неверно";
--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,24 @@
+#include <tgbot/tgbot.h>
+
 #include <cstring>
 #include <iostream>
 
 #include "headers/config.h"
+#include "headers/messages.h"
 
 using namespace std;
+using namespace TgBot;
 
-int main(int argc, char const *argv[]) {
+int main(int argc, char const* argv[]) {
   setlocale(LC_ALL, "ru");
 
-  // проверяю указан ли токен
+  // проверяю минимальное кол-во аргументов
   if (argc < 3) {
-    cout << "\x1b[31mError: \x1b[0m"
-         << "Токен не был указан в качестве параметра" << endl;
+    cout << "\x1b[31mError: \x1b[0m" << ERROR__TOKEN_UNDEFINED << endl;
     exit(1);
   }
+
+  bool DEFAULT = false;
 
   // перебираю флаги и аргументы
   for (int arg = 1; arg < argc; arg++) {
@@ -24,11 +29,36 @@ int main(int argc, char const *argv[]) {
       if (argv[arg + 1][0] != '-') {
         TOKEN = argv[arg + 1];
       } else {
-        cout << "\x1b[31mError: \x1b[0m"
-             << "Токен указан неверно" << endl;
+        cout << "\x1b[31mError: \x1b[0m" << ERROR__TOKEN_INCORRECTLY << endl;
         exit(1);
       }
+      // если найден флаг дефолтного подключения
+    } else if (strcmp(argv[arg], DEFAULT_FLAG__SHORT) == 0 ||
+               strcmp(argv[arg], DEFAULT_FLAG) == 0) {
+      DEFAULT = true;
     }
+  }
+
+  // инициализация бота
+  Bot bot(TOKEN);
+
+  // пробная обработка команд
+  bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
+    bot.getApi().sendMessage(message->chat->id, "Hi!");
+  });
+
+  try {
+    cout << "Bot " << bot.getApi().getMe()->username.c_str() << "is found"
+         << endl;
+    bot.getApi().deleteWebhook();
+
+    TgLongPoll longPoll(bot);
+    while (true) {
+      cout << "Check for updates..." << endl;
+      longPoll.start();
+    }
+  } catch (exception& e) {
+    cout << "\x1b[31mError: \x1b[0m" << e.what() << endl;
   }
 
   return 0;
