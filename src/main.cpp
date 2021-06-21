d858929713ce977efff424449b7c5ad3c0d1d661
egorsobolev2000 <web-disain@mail.ua>
2021-06-21 18:17:59 +0300

    Реализация обработки флага и параметра токена

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,35 @@
+#include <cstring>
 #include <iostream>
+
+#include "headers/config.h"
 
 using namespace std;
 
 int main(int argc, char const *argv[]) {
-  /* code */
+  setlocale(LC_ALL, "ru");
+
+  // проверяю указан ли токен
+  if (argc < 3) {
+    cout << "\x1b[31mError: \x1b[0m"
+         << "Токен не был указан в качестве параметра" << endl;
+    exit(1);
+  }
+
+  // перебираю флаги и аргументы
+  for (int arg = 1; arg < argc; arg++) {
+    // если найден флаг токена
+    if (strcmp(argv[arg], TOKEN_FLAG__SHORT) == 0 ||
+        strcmp(argv[arg], TOKEN_FLAG) == 0) {
+      // проверяю корректно ли указан аргумент флага
+      if (argv[arg + 1][0] != '-') {
+        TOKEN = argv[arg + 1];
+      } else {
+        cout << "\x1b[31mError: \x1b[0m"
+             << "Токен указан неверно" << endl;
+        exit(1);
+      }
+    }
+  }
+
   return 0;
 }
