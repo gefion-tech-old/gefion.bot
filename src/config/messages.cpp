79301d935e653aa0eb38bbab9dff3025919b44b1
egorsobolev2000 <web-disain@mail.ua>
2021-06-23 18:46:18 +0300

    Доработка

--- src/config/messages.cpp
+++ src/config/messages.cpp
@@ -1,2 +1,4 @@
-char ERROR__TOKEN_UNDEFINED[] = "Токен не был указан в качестве параметра";
-char ERROR__TOKEN_INCORRECTLY[] = "Токен указан неверно";
+#include "../headers/messages.h"
+
+char msg::ERROR__TOKEN_UNDEFINED[] = "Токен не был указан в качестве параметра";
+char msg::ERROR__TOKEN_INCORRECTLY[] = "Токен указан неверно";
--- src/main.cpp
+++ src/main.cpp
@@ -2,9 +2,12 @@
 
 #include <cstring>
 #include <iostream>
+#include <string>
 
 #include "headers/config.h"
+#include "headers/externalTools.h"
 #include "headers/messages.h"
+#include "headers/params.h"
 
 using namespace std;
 using namespace TgBot;
@@ -12,43 +15,61 @@ using namespace TgBot;
 int main(int argc, char const* argv[]) {
   setlocale(LC_ALL, "ru");
 
+  string TOKEN;
+
   // проверяю минимальное кол-во аргументов
-  if (argc < 3) {
-    cout << "\x1b[31mError: \x1b[0m" << ERROR__TOKEN_UNDEFINED << endl;
+  if (argc < 2) {
+    PrintError(msg::ERROR__TOKEN_UNDEFINED);
     exit(1);
   }
 
-  bool DEFAULT = false;
+  // bool DEFAULT = false;
 
   // перебираю флаги и аргументы
   for (int arg = 1; arg < argc; arg++) {
     // если найден флаг токена
-    if (strcmp(argv[arg], TOKEN_FLAG__SHORT) == 0 ||
-        strcmp(argv[arg], TOKEN_FLAG) == 0) {
-      // проверяю корректно ли указан аргумент флага
-      if (argv[arg + 1][0] != '-') {
-        TOKEN = argv[arg + 1];
+    if (strcmp(argv[arg], params::TOKEN_FLAG__SHORT) == 0 ||
+        strcmp(argv[arg], params::TOKEN_FLAG) == 0) {
+      // проверяю указан ли параметр
+      if (argc >= 3) {
+        // проверяю корректно ли указан аргумент флага
+        if (argv[arg + 1][0] != '-') {
+          TOKEN = argv[arg + 1];
+        } else {
+          PrintError(msg::ERROR__TOKEN_INCORRECTLY);
+          exit(1);
+        }
       } else {
-        cout << "\x1b[31mError: \x1b[0m" << ERROR__TOKEN_INCORRECTLY << endl;
+        PrintError(msg::ERROR__TOKEN_INCORRECTLY);
         exit(1);
       }
-      // если найден флаг дефолтного подключения
-    } else if (strcmp(argv[arg], DEFAULT_FLAG__SHORT) == 0 ||
-               strcmp(argv[arg], DEFAULT_FLAG) == 0) {
-      DEFAULT = true;
+
+      // если присутствует флаг дефолтного подключения
+    } else if (strcmp(argv[arg], params::DEFAULT_FLAG__SHORT) == 0 ||
+               strcmp(argv[arg], params::DEFAULT_FLAG) == 0) {
+      // DEFAULT = true;
+    } else if (strcmp(argv[arg], params::HELP_FLAG__SHORT) == 0 ||
+               strcmp(argv[arg], params::HELP_FLAG) == 0) {
+      ShowHelpInfo();
+      exit(1);
     }
   }
+
+  // очищаю консоль
+  system("clear");
 
   // инициализация бота
   Bot bot(TOKEN);
 
-  // пробная обработка команд
+  //пробная обработка команд
   bot.getEvents().onCommand("start", [&bot](Message::Ptr message) {
     bot.getApi().sendMessage(message->chat->id, "Hi!");
   });
 
   try {
-    cout << "Bot " << bot.getApi().getMe()->username.c_str() << "is found"
+    cout << "Bot "
+         << "\x1b[33m" << bot.getApi().getMe()->username.c_str() << "\x1b[0m"
+         << " is found." << endl
          << endl;
     bot.getApi().deleteWebhook();
 
@@ -58,7 +79,7 @@ int main(int argc, char const* argv[]) {
       longPoll.start();
     }
   } catch (exception& e) {
-    cout << "\x1b[31mError: \x1b[0m" << e.what() << endl;
+    PrintError(e.what());
   }
 
   return 0;
